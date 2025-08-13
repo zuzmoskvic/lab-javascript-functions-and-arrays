9d7ab833afb1b1edb0a9b1ea31119c532cf8ae45
“Zuzana <zuzana.moskvic@gmail.com>
2025-08-13 16:41:19 +0200

    Iteration 4

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -43,11 +43,33 @@ function findLongestWord(words) {
 function sumNumbers(numbers) {
     // let sum = 0;
     // for (let number of numbers) { sum += number }
-    
+
     if (numbers.length === 0)  return 0 ;
     return numbers.reduce((sum, total) =>{
         return sum + total 
     }, 0
     );
 
+}
+
+// ### Iteration #4: Calculate the average
+
+// Calculating an average is a very common task. Let's practice it a bit.
+
+// **The logic behind this:**
+
+// 1. Find the sum as we did in the first exercise (or how about reusing the function `sumNumbers()`?)
+// 2. Take that sum and divide it by the number of elements in the array.
+
+// <br>
+
+function averageNumbers(array) {
+    let sum = sumNumbers(array);
+    if (array.length === 0 ) {
+        average = null
+    }
+    else {
+        average = sum / array.length;
+    }
+    return average;
 }
